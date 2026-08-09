c9cb43efb41c516d55d22de7d024e483c12a5812
Alperen Karacan <alperenkaracan35@gmail.com>
2026-08-09 22:15:34 +0300

    feat(api-client): generate typed frontend SDK

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,7 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'coverage'] },
+  { ignores: ['dist', 'coverage', 'src/api/generated'] },
   {
     extends: [
       js.configs.recommended,
